5642086c7d028bf725fea1c3e6c312af88072569
Vladox69 <marwladiagua2001@hotmail.com>
2023-05-05 14:17:03 -0500

    Creación de datos

---
+++ poke-app/src/pokemons/data/pokemons.js
@@ -0,0 +1,102 @@
+export const pokemons = [
+    {
+        'id': '1',
+        'pokemon':'bulbasaur',
+        'types':'Planta,Veneno'
+    },
+    {
+        'id': '2',
+        'pokemon':'ivysaur',
+        'types':'Planta,Veneno'
+    },
+    {
+        'id': '3',
+        'pokemon':'venusaur',
+        'types':'Planta,Veneno'
+    },
+    {
+        'id': '4',
+        'pokemon':'charmander',
+        'types':'Fuego'
+    },
+    {
+        'id': '5',
+        'pokemon':'charmeleon',
+        'types':'Fuego'
+    },
+    {
+        'id': '6',
+        'pokemon':'charizard',
+        'types':'Fuego,Volador'
+    },
+    {
+        'id': '7',
+        'pokemon':'squirtle',
+        'types':'Agua'
+    },
+    {
+        'id': '8',
+        'pokemon':'wartortle',
+        'types':'Agua'
+    },
+    {
+        'id': '9',
+        'pokemon':'blastoise',
+        'types':'Agua'
+    },
+    {
+        'id': '10',
+        'pokemon':'caterpie',
+        'types':'Bicho'
+    },
+    {
+        'id': '11',
+        'pokemon':'metapod',
+        'types':'Bicho'
+    },
+    {
+        'id': '12',
+        'pokemon':'butterfree',
+        'types':'Bicho,Volador'
+    },
+    {
+        'id': '13',
+        'pokemon':'weedle',
+        'types':'Bicho,Veneno'
+    },
+    {
+        'id': '14',
+        'pokemon':'kakuna',
+        'types':'Bicho,Veneno'
+    },
+    {
+        'id': '15',
+        'pokemon':'beedrill',
+        'types':'Bicho,Veneno'
+    },
+    {
+        'id': '16',
+        'pokemon':'pidgey',
+        'types':'Normal,Volador'
+    },
+    {
+        'id': '17',
+        'pokemon':'pidgeotto',
+        'types':'Normal,Volador'
+    },
+    {
+        'id': '18',
+        'pokemon':'pidgeot',
+        'types':'Normal,Volador'
+    },
+    {
+        'id': '19',
+        'pokemon':'rattata',
+        'types':'Normal'
+    },
+    {
+        'id': '20',
+        'pokemon':'raticate',
+        'types':'Normal'
+    },
+]
